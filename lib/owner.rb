086928c0b170bc55795273e42ee5c57c088b03a8
Warren Yao <warrenyca905@gmail.com>
2019-06-06 14:07:11 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
 class Owner
-  # code goes here
+  attr_accessor :name
+  attr_reader :species
+  
+  @@all = []
+  
+  def initialize(name)
+    Owner.all << self
+    @species = "human"
+    @name = name
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+  def pets
+    {
+      :fishes => [],
+      :dogs => [],
+      :cats => []}
+  end
+  
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    self.pets[:fishes] << new_fish
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    self.all.length
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
 end
